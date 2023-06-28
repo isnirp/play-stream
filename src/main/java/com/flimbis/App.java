e99429211ee3f5fca45af45d8d02887d5d02d429
isnirp <pramankwah@gmail.com>
2023-06-28 11:42:03 +0200

    init app

---
+++ src/main/java/com/flimbis/App.java
@@ -0,0 +1,13 @@
+package com.flimbis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
