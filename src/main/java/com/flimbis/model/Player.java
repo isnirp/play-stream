ca40264f95209f48ba13686557682fe756ccb745
isnirp <pramankwah@gmail.com>
2023-06-30 13:47:24 +0200

    update player model

--- src/main/java/com/flimbis/model/Player.java
+++ src/main/java/com/flimbis/model/Player.java
@@ -2,4 +2,17 @@ package com.flimbis.model;
 
 public class Player {
     private String userName;
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
+
