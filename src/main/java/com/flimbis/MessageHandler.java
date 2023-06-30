b062e20b848801b36f9fcbfe87544ad2e21755cd
isnirp <pramankwah@gmail.com>
2023-06-30 13:48:39 +0200

    define message handler

--- src/main/java/com/flimbis/MessageHandler.java
+++ src/main/java/com/flimbis/MessageHandler.java
@@ -1,4 +1,36 @@
 package com.flimbis;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+ *  keep track of messages
+ *  set cap and keep count
+ *
+ * */
 public class MessageHandler {
+    public static final int MESSAGE_CAP = 4;
+    public static final String MESSAGE_INIT = "mango";
+    private final AtomicInteger counter;
+    private String message;
+
+
+    public MessageHandler() {
+        this.counter = new AtomicInteger(0);
+    }
+
+    public AtomicInteger getCounter() {
+        return counter;
+    }
+
+    public void keepCount() {
+        counter.incrementAndGet();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message + "-" + getCounter();
+    }
 }
--- src/test/java/com/flimbis/MessageHandlerTest.java
+++ src/test/java/com/flimbis/MessageHandlerTest.java
@@ -1,6 +1,19 @@
 package com.flimbis;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MessageHandlerTest {
-  
+
+    @Test
+    void testMsgAppendsMsgCounter() {
+        MessageHandler messageHandler = new MessageHandler();
+
+        messageHandler.keepCount();
+        messageHandler.setMessage(MessageHandler.MESSAGE_INIT);
+
+        String actual = MessageHandler.MESSAGE_INIT + "-" + messageHandler.getCounter();
+        assertThat(messageHandler.getMessage()).isEqualTo(actual);
+    }
 }
